d04efc7a480e4ecfcd46ea1fcfcc0c9bb6abea14
Matthias Knoll <matthias@bravenewtalent.com>
2012-11-09 11:59:45 +0000

    fix
    Signed-off-by: Matthias Knoll <matthias@bravenewtalent.com>

--- lib/src/results.dart
+++ lib/src/results.dart
@@ -30,7 +30,9 @@ class ResultsIterator implements Iterator<dynamic> {
   
   ResultsIterator._internal(Results this._results);
   
-  bool get hasNext => i < _results._dataPackets.length;
+  bool hasNext() {
+    return i < _results._dataPackets.length;
+  }
   
   List<dynamic> next() => _results._dataPackets[i++].values;
 }
